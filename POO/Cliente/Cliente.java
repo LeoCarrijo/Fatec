6f34de428ab2875e8c0d08d68533a1bc13d5a0a7
LeoCarrijo <lh83712@gmail.com>
2024-03-03 11:38:36 -0300

    Arquivos do POO

---
+++ POO/Cliente/Cliente.java
@@ -0,0 +1,32 @@
+package Web.Cliente;
+
+public class Cliente {
+    private int numeroConta, numeroAgencia;
+    private String nome;
+    private float saldo;
+
+    public Cliente(int numeroConta, int numeroAgencia, String nome, float saldo){
+        this.numeroConta = numeroConta;
+        this.numeroAgencia = numeroAgencia;
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+
+    public void mostrarInformacoes(){
+        System.out.println("\nNome: " + this.nome +
+                "\nNº da Conta: " + this.numeroConta +
+                "\nSaldo Atual: " + this.saldo);
+    }
+
+    public void realizarDeposito(float quantia){
+        this.saldo += quantia;
+        System.out.println("\nR$" + quantia + " depositado(s)" +
+                "\nSaldo Atual: " + this.saldo);
+    }
+
+    public void realizarSaque(float quantia){
+        this.saldo -= quantia;
+        System.out.println("\nR$" + quantia + " Sacado(s)" +
+                "\nSaldo Atual: " + this.saldo);
+    }
+}
---
+++ POO/Rio/TestaRio.java
@@ -0,0 +1,22 @@
+package Web.Rio;
+
+public class TestaRio {
+    public static void main(String[] args) {
+        Rio rio1 = new Rio("Amazonas", 70, false);
+        Rio rio2 = new Rio("Tietê", 50, true);
+
+        rio1.mostra();
+        rio1.chover(50);
+        rio1.ensolarar(2);
+        rio1.sujar();
+        rio1.limpar();
+        rio1.mostra();
+
+        rio2.mostra();
+        rio2.chover(70);
+        rio2.ensolarar(4);
+        rio2.limpar();
+        rio2.sujar();
+        rio2.mostra();
+    }
+}
