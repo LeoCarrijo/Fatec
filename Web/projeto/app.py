fc73c1205713f283b26ce293013c1b584d182226
Leonardo Carrijo <86086793+LeoCarrijo@users.noreply.github.com>
2024-03-06 20:07:02 -0300

    Aula dia 06/03/24

--- Web/projeto/app.py
+++ Web/projeto/app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return "<h1>Foda-se</h1>"
+def root():
+    return render_template('index.html')
+
+@app.route('/sobre')
+def about():
+    return render_template('about.html')
+
+@app.route('/contato')
+def contact():
+    return render_template('contato.html')
+
+@app.route('/servicos')
+def services():
+    return render_template('servicos.html')
 
 app.run()
