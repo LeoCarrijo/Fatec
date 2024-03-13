282986cab6603564c16d7ff35b0c6d1f5ceb3aeb
Leonardo Carrijo <86086793+LeoCarrijo@users.noreply.github.com>
2024-03-12 21:58:43 -0300

    Arquivos Aula IMC 2

--- Web/aula0703/app.py
+++ Web/aula0703/app.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def root():
     return render_template('index.html')
+
+@app.route('/calculadorIMC', methods=['POST'])
+def calculadorIMC():
+    altura = float(request.form['altura'])
+    peso = float(request.form['peso'])
+    imc = peso / (altura * altura)
+    return render_template('index.html', var_imc=imc)
 
 app.run()
