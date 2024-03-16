25daec8f4065739ca63cd5435fab46d43ea83f95
LeoCarrijo <lh83712@gmail.com>
2024-03-16 18:13:42 -0300

    Atts

--- poo/src/lista3/rede/Mensagem.java
+++ poo/src/lista3/rede/Mensagem.java
@@ -1,4 +1,37 @@
 package lista3.rede;
 
 public class Mensagem extends Pessoa{
+    private int idm;
+    private String text;
+
+    public int getIdm() {
+        return idm;
+    }
+
+    public void setIdm(int idm) {
+        this.idm = idm;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Mensagem(int id, String nome, String usuario, int idm, String text) {
+        super(id, nome, usuario);
+        this.idm = idm;
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Mensagem{" +
+                "idm=" + idm +
+                ", text='" + text + '\'' +
+                super.toString() +
+                '}';
+    }
 }
--- poo/src/lista3/rede/Pessoa.java
+++ poo/src/lista3/rede/Pessoa.java
@@ -4,9 +4,42 @@ public class Pessoa {
     protected int id;
     protected String nome, usuario;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
     public Pessoa(int id, String nome, String usuario) {
         this.id = id;
         this.nome = nome;
         this.usuario = usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", usuario='" + usuario + '\'' +
+                '}';
     }
 }
