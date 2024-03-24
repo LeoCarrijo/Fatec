9a8f03b02b2cf92b3616914b90b5d5b1581a95ca
LeoCarrijo <lh83712@gmail.com>
2024-03-24 17:47:21 -0300

    Atts

--- Web/pratica/Scripts/activate.bat
+++ Web/pratica/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\lh837\Documents\RepositoriosClones\Fatec\Web\pratica
+set VIRTUAL_ENV=C:\Users\lh837\Documentos\Sistemas\GitHub\Fatec\Web\pratica
 
 if not defined PROMPT set PROMPT=$P$G
 
