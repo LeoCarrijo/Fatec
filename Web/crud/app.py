03552b3b90f94e877203e83ce183a61131e633ab
Leonardo Carrijo <86086793+LeoCarrijo@users.noreply.github.com>
2024-04-18 20:20:14 -0300

    cadusuario.html e conexao com DB

--- Web/crud/app.py
+++ Web/crud/app.py
@@ -1,9 +1,41 @@
 from flask import Flask, request, render_template
+import mysql.connector
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return render_template('index.html')
+
+@app.route('/cadastrar_usuario', methods=['POST'])
+def inserir_usuario():
+    nome = request.form['nome']
+    cpf = request.form['cpf']
+    email = request.form['email']
+    senha = request.form['senha']
+    db = mysql.connector.connect(host='201.23.3.86',
+                                 port=5000,
+                                 user='user_aluno',
+                                 password='E$tud@_m@1$',
+                                 database='aula_fatec')
+    mycursor = db.cursor()
+    query = "INSERT INTO leocarrijotbusuario (nome, cpf, email, senha) VALUES (%s, %s, %s, %s)"
+    values = (nome, cpf, email, senha)
+    mycursor.execute(query,values)
+    db.commit()
+    return 'gravou'
+
+@app.route('/caduser')
+def lista_user():
+    db = mysql.connector.connect(host='201.23.3.86',
+                                 port=5000,
+                                 user='user_aluno',
+                                 password='E$tud@_m@1$',
+                                 database='aula_fatec')
+    mycursor = db.cursor()
+    query = 'select nome, cpf, email from leocarrijo_tbusuario'
+    mycursor.execute(query)
+    resultado = mycursor.fetchall()
+    return render_template('cadusuario.html', usuarios=resultado)
 
 app.run()
