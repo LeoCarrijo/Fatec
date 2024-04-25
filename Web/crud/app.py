8dc832971483b0b9d5309792b125c707068198ac
Leonardo Carrijo <86086793+LeoCarrijo@users.noreply.github.com>
2024-04-25 20:28:14 -0300

    Usando apenas uma página

--- Web/crud/app.py
+++ Web/crud/app.py
@@ -1,55 +1,50 @@
-
-from flask import Flask, request, render_template
+from flask import Flask,render_template, request, redirect, url_for
 import mysql.connector
 
 app = Flask(__name__)
 
-@app.route('/')
-def root():
-    return render_template('index.html')
+@app.route("/")
+def raiz():
+    return render_template('cadusuario.html')
 
-@app.route('/cadastrar_usuario', methods=['POST'])
+@app.route('/cadastrar_usuario',methods=['POST'])
 def inserir_usuario():
-    nome = request.form['nome']
-    cpf = request.form['cpf']
-    email = request.form['email']
-    senha = request.form['senha']
-    db = mysql.connector.connect(host='201.23.3.86',
-                                 port=5000,
-                                 user='usr_aluno',
-                                 password='E$tud@_m@1$',
-                                 database='aula_fatec')
+    nome = request.form['txt_nome']
+    cpf = request.form['txt_cpf']
+    email = request.form['txt_email']
+    senha = request.form['txt_senha']
+    db = mysql.connector.connect(host='201.23.3.86', port=5000, user='usr_aluno',password='E$tud@_m@1$',database='aula_fatec')
     mycursor = db.cursor()
-    query = "INSERT INTO leocarrijo_tbusuario (nome, cpf, email, senha) VALUES (%s, %s, %s, %s)"
+    query = "INSERT INTO leonardo_tbusuario (nome, cpf, email, senha) VALUES (%s,%s,%s,%s)"
     values = (nome, cpf, email, senha)
     mycursor.execute(query,values)
     db.commit()
     return 'gravou'
 
-@app.route('/caduser')
+@app.route('/usercad')
 def lista_user():
     db = mysql.connector.connect(host='201.23.3.86',
-                                 port=5000,
-                                 user='usr_aluno',
-                                 password='E$tud@_m@1$',
-                                 database='aula_fatec')
+                                port=5000,
+                                user='usr_aluno',
+                                password='E$tud@_m@1$',
+                                database='aula_fatec')
     mycursor = db.cursor()
     query = 'select nome, cpf, email from leocarrijo_tbusuario'
     mycursor.execute(query)
     resultado = mycursor.fetchall()
-    return render_template('cadusuario.html', usuarios=resultado)
+    return render_template('cadusuario.html', opcao='listar', usuarios= resultado)
 
-@app.route('/alterar_usuario/<user>')
+@app.route('/alterar_usuario/<user>') #Carregar a página de cadastro
 def alterar_usuario(user):
     db = mysql.connector.connect(host='201.23.3.86',
-                                 port=5000,
-                                 user='usr_aluno',
-                                 password='E$tud@_m@1$',
-                                 database='aula_fatec')
+                                port=5000,
+                                user='usr_aluno',
+                                password='E$tud@_m@1$',
+                                database='aula_fatec')
     mycursor = db.cursor()
-    query = 'select nome, cpf, email from leocarrijo_tbusuario where id = ' + user
+    query = 'select nome, cpf, email, id from leocarrijo_tbusuario where id = ' + user
     mycursor.execute(query)
     resultado = mycursor.fetchall()
-    return render_template('cadusuario.html', usuarios=resultado)
+    return render_template('cadusuario.html', usuarios= resultado)
 
 app.run()
