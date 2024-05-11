1d35cda6ddf33d32f7b32b9f5757bf4bd9fb82d4
Leonardo Carrijo <86086793+LeoCarrijo@users.noreply.github.com>
2024-05-11 14:25:10 -0300

    Atualização da Pagina Flask

--- Loja/src/main/java/fatecfranca/edu/loja/ClienteService.java
+++
@@ -1,5 +0,0 @@
-package fatecfranca.edu.loja;
-
-public class ClienteService {
-    
-}
---
+++ Loja/src/main/java/fatecfranca/edu/loja/controller/ClienteController.java
@@ -0,0 +1,20 @@
+package fatecfranca.edu.loja.controller;
+
+import fatecfranca.edu.loja.model.entity.ClienteEntity;
+import fatecfranca.edu.loja.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Responde pelas requisições rest
+@RequestMapping("/cliente") // no endpoint /cliente
+public class ClienteController {
+    @Autowired
+    ClienteService injecao;
+    @PostMapping
+    public ClienteEntity insere(@RequestBody ClienteEntity cliente){
+        return injecao.insere(cliente);
+    }
+}
