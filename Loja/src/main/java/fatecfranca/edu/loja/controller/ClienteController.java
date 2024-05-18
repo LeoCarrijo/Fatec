ed7184641f5f1d3e7ecd88cd5b6d9d7717be8ac0
Leonardo Carrijo <86086793+LeoCarrijo@users.noreply.github.com>
2024-05-17 21:33:05 -0300

    :sparkles: feat: GET que consulta por ID

--- Loja/src/main/java/fatecfranca/edu/loja/controller/ClienteController.java
+++ Loja/src/main/java/fatecfranca/edu/loja/controller/ClienteController.java
@@ -4,10 +4,9 @@ import fatecfranca.edu.loja.model.dto.ClienteDTO;
 import fatecfranca.edu.loja.model.entity.ClienteEntity;
 import fatecfranca.edu.loja.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController // Responde pelas requisições rest
 @RequestMapping("/cliente") // no endpoint /cliente
@@ -17,5 +16,15 @@ public class ClienteController {
     @PostMapping
     public ClienteDTO insere(@RequestBody ClienteDTO clienteDTO){
         return injecao.insere(clienteDTO);
+    }
+
+    @GetMapping
+    public List<ClienteDTO> consultaTodos() {
+        return injecao.consultaTodos();
+    }
+
+    @GetMapping("{id}")
+    public ClienteDTO consultaPorId(@PathVariable Long id) {
+        return injecao.consultaPorId(id);
     }
 }
--- Loja/src/main/java/fatecfranca/edu/loja/service/ClienteService.java
+++ Loja/src/main/java/fatecfranca/edu/loja/service/ClienteService.java
@@ -6,6 +6,10 @@ import fatecfranca.edu.loja.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ClienteService {
     // injeção de dependência
@@ -15,6 +19,34 @@ public class ClienteService {
         ClienteEntity clienteEntity = converteDTO(clienteDTO);
         // o converteEntity retornado pelo método save foi convertido em DTO
         return converteEntity(injecao.save(clienteEntity));
+    }
+
+    // Recupera um cliente a partir de um id
+    public ClienteDTO consultaPorId(Long id) {
+        Optional<ClienteEntity> cliente = injecao.findById(id);
+        // verificar se tem cliente na resposta
+        if (cliente.isPresent()) {
+            return converteEntity(cliente.get()); // retorna o cliente entity encontrado
+        }
+        return null; // informa que não encontrou
+    }
+
+    // Consulta e trazer uma lista de clientes cadastrados no banco de dados
+    public List<ClienteDTO> consultaTodos(){
+        // Precisamos converter uma lista de clienteEntity em outra lista
+        // de clienteDTO
+        return converteEntities(injecao.findAll());
+    }
+
+    // Converte uma lista de ClienteEntity em outra lista de ClienteDTO
+    public List<ClienteDTO> converteEntities(List<ClienteEntity> listaEntities) {
+        // Cria uma lista que vai guardar os ClienteDTOs
+        List<ClienteDTO> listaDTOs = new ArrayList<ClienteDTO>();
+        // Percorre a lista de ClienteEntity
+        for(ClienteEntity objEntity : listaEntities){
+            listaDTOs.add(converteEntity(objEntity));
+        }
+        return listaDTOs;
     }
 
     // Converte ClienteDTO para ClienteEntity
