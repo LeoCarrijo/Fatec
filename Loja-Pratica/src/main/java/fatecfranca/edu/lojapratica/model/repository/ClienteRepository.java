b1c9b393d62381bb764e1f8a8d6815761a5e0a2a
LeoCarrijo <lh83712@gmail.com>
2024-05-18 19:56:27 -0300

    :boom: fix: Corrigindo Erro no 'injecao' no 'ClienteService'

--- Loja-Pratica/src/main/java/fatecfranca/edu/lojapratica/model/repository/ClienteRepository.java
+++ Loja-Pratica/src/main/java/fatecfranca/edu/lojapratica/model/repository/ClienteRepository.java
@@ -1,8 +1,9 @@
 package fatecfranca.edu.lojapratica.model.repository;
 
+import fatecfranca.edu.lojapratica.model.entity.ClienteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository<ClienteRepository, Long> {
+public interface ClienteRepository extends JpaRepository<ClienteEntity, Long> {
     // esta interface vai herdar todos os métodos de CRUD
     // (Create, read, update, delete) de ClienteEntity
     // não vamos precisar de escrever código SQL
