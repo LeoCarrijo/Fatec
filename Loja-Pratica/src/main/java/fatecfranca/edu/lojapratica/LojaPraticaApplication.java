cfa626776ff9f3d74cd880a439fb433dda985dc8
LeoCarrijo <lh83712@gmail.com>
2024-05-18 14:36:11 -0300

    :tada: Criação de Projeto para Prática

---
+++ Loja-Pratica/src/main/java/fatecfranca/edu/lojapratica/LojaPraticaApplication.java
@@ -0,0 +1,13 @@
+package fatecfranca.edu.lojapratica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaPraticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LojaPraticaApplication.class, args);
+    }
+
+}
---
+++ Loja-Pratica/src/test/java/fatecfranca/edu/lojapratica/LojaPraticaApplicationTests.java
@@ -0,0 +1,13 @@
+package fatecfranca.edu.lojapratica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaPraticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
