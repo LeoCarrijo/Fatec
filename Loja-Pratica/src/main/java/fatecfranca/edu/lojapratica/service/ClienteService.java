68fb675a18f67c95aefe8cfd1f805edef760167c
LeoCarrijo <lh83712@gmail.com>
2024-05-18 19:41:37 -0300

    :sparkles: feat: Criando Métodos para Conversão de Lista Entities para DTOs

--- Loja-Pratica/src/main/java/fatecfranca/edu/lojapratica/service/ClienteService.java
+++ Loja-Pratica/src/main/java/fatecfranca/edu/lojapratica/service/ClienteService.java
@@ -6,6 +6,10 @@ import fatecfranca.edu.lojapratica.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ClienteService {
     @Autowired
@@ -13,6 +17,26 @@ public class ClienteService {
     public ClienteDTO insere(ClienteDTO clienteDTO) {
         ClienteEntity clienteEntity = converteParaEntity(clienteDTO);
         return converteParaDTO(injecao.save(clienteEntity));
+    }
+
+    public List<ClienteDTO> consultaTodos() {
+        return converteEntitiesParaDTOs(injecao.findAll());
+    }
+
+    public List<ClienteDTO> converteEntitiesParaDTOs(List<ClienteEntity> clienteEntities) {
+        List<ClienteDTO> clienteDTOs = new ArrayList<ClienteDTO>();
+        for(ClienteEntity clienteEntity : clienteEntities){
+            clienteDTOs.add(converteParaDTO(clienteEntity));
+        }
+        return clienteDTOs;
+    }
+
+    public ClienteDTO consultaPorId(Long id) {
+        Optional<ClienteEntity> cliente = injecao.findById(id);
+        if(cliente.isPresent()) {
+            return converteParaDTO(cliente.get());
+        }
+        return null;
     }
 
     public ClienteEntity converteParaEntity(ClienteDTO clienteDTO) {
