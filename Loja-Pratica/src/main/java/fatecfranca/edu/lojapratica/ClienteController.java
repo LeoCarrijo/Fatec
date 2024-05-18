673c5d86c5e9ce7d3dae804e47a0edcd603fdb20
LeoCarrijo <lh83712@gmail.com>
2024-05-18 14:57:51 -0300

    :sparkles: feat: Crição do Arquivo 'ClienteController'

---
+++ Loja-Pratica/src/main/java/fatecfranca/edu/lojapratica/ClienteController.java
@@ -0,0 +1,20 @@
+package fatecfranca.edu.lojapratica;
+
+import fatecfranca.edu.lojapratica.model.entity.ClienteEntity;
+import fatecfranca.edu.lojapratica.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+    @Autowired
+    ClienteService injecao;
+    @PostMapping
+    public ClienteEntity insere(@RequestBody ClienteEntity cliente) {
+        return injecao.insere(cliente);
+    }
+}
