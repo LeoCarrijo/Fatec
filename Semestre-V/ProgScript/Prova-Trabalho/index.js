800c8c8ef22e39b5af3877f584e1ef8471071c49
LeoCarrijo <lh83712@gmail.com>
2024-09-06 23:59:36 -0300

    :sparkles: feat: Métodos de Attack

--- Semestre-V/ProgScript/Prova-Trabalho/index.js
+++ Semestre-V/ProgScript/Prova-Trabalho/index.js
@@ -5,11 +5,13 @@ createApp({
         return {
             hero: {
                 life: 100,
-                name: 'Guts'
+                name: 'Guts',
+                maxDmg: 10
             },
             villan: {
                 life: 100,
-                name: 'Griffith'
+                name: 'Griffith',
+                maxDmg: 10
             }
         }
     },
@@ -17,7 +19,7 @@ createApp({
         handlerClick(opt) {
             switch(opt) {
                 case 'attack':
-                    this.attack()
+                    this.attack(true)
                 case 'defend':
                     this.defend()
                 case 'use':
@@ -26,8 +28,18 @@ createApp({
                     this.flee()
             }
         },
-        attack() {
-            console.log(this.generateRng(10))
+        attack(isHero) {
+            if(isHero) {
+                let dmg = this.generateRng(this.hero.maxDmg)
+                this.causeDamage(this.villan.life, dmg, isHero)
+                console.log(`${dmg} de dano causado`)
+                console.log(`Vida de ${this.villan.name}: ${this.villan.life}`)
+            } else {
+                let dmg = this.generateRng(this.villan.maxDmg)
+                this.causeDamage(this.villan.life, dmg, isHero)
+                console.log(`${dmg} de dano causado`)
+                console.log(`Vida de ${this.hero.name}: ${this.hero.life}`)
+            }
         },
         defend() {
 
@@ -40,6 +52,19 @@ createApp({
         },
         generateRng(max) {
             return (Math.floor(Math.random() * max))
+        },
+        causeDamage(life, dmg, isHero) {
+            if(isHero) {
+                this.villan.life -= dmg
+                if(this.villan.life < 0) {
+                    this.villan.life = 0
+                }
+            } else {
+                this.hero.ife -= dmg
+                if(this.hero.life < 0) {
+                    this.hero.life = 0
+                }
+            }
         }
         // attack(isHero) {
 
