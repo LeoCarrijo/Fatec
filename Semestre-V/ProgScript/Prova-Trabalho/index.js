b9f2d974c3e0307207deed6951f29d77c38ff3d4
LeoCarrijo <lh83712@gmail.com>
2024-09-07 00:37:29 -0300

    :sparkles: feat: Mecânica de defesa

--- Semestre-V/ProgScript/Prova-Trabalho/index.js
+++ Semestre-V/ProgScript/Prova-Trabalho/index.js
@@ -6,12 +6,16 @@ createApp({
             hero: {
                 life: 100,
                 name: 'Guts',
-                maxDmg: 10
+                maxDmg: 10,
+                defended: false,
+                defenseArmor: 2
             },
             villan: {
                 life: 100,
                 name: 'Griffith',
-                maxDmg: 10
+                maxDmg: 10,
+                defended: false,
+                defenseArmor: 2
             }
         }
     },
@@ -20,29 +24,41 @@ createApp({
             switch(opt) {
                 case 'attack':
                     this.attack(true)
+                    break
                 case 'defend':
-                    this.defend()
+                    this.defend(true)
+                    break
                 case 'use':
                     this.use()
+                    break
                 case 'flee':
                     this.flee()
+                    break
             }
         },
         attack(isHero) {
             if(isHero) {
                 let dmg = this.generateRng(this.hero.maxDmg)
-                this.causeDamage(this.villan.life, dmg, isHero)
+                this.causeDamage(dmg, isHero)
                 console.log(`${dmg} de dano causado`)
                 console.log(`Vida de ${this.villan.name}: ${this.villan.life}`)
             } else {
                 let dmg = this.generateRng(this.villan.maxDmg)
-                this.causeDamage(this.villan.life, dmg, isHero)
+                this.causeDamage(dmg, isHero)
                 console.log(`${dmg} de dano causado`)
                 console.log(`Vida de ${this.hero.name}: ${this.hero.life}`)
             }
         },
-        defend() {
-
+        defend(isHero) {
+            if(isHero) {
+                this.hero.defend = true
+                this.hero.defenseArmor = 2
+                console.log(`${this.hero.name} está defendendo com ${this.hero.defenseArmor} de armadura`)
+            } else {
+                this.villan.defend = true
+                this.villan.defenseArmor = 2
+                console.log(`${this.villan.name} está defendendo com ${this.villan.defenseArmor} de armadura`)
+            }
         },
         use() {
 
@@ -51,18 +67,38 @@ createApp({
 
         },
         generateRng(max) {
-            return (Math.floor(Math.random() * max))
+            let value = (Math.floor(Math.random() * max))
+            if(value == 0) {
+                return 1
+            } else {
+                return value
+            }
         },
         causeDamage(dmg, isHero) {
             if(isHero) {
-                this.villan.life -= dmg
-                if(this.villan.life < 0) {
-                    this.villan.life = 0
+                if(!this.villan.defend) {
+                    this.villan.life -= dmg
+                    if(this.villan.life < 0) {
+                        this.villan.life = 0
+                    }
+                } else {
+                    this.villan.defenseArmor -= 1
+                    console.log(`${this.villan.defenseArmor} de armadura restante`)
+                    if(this.villan.defenseArmor == 0) {
+                        this.villan.defend = false
+                    }
                 }
             } else {
-                this.hero.ife -= dmg
-                if(this.hero.life < 0) {
-                    this.hero.life = 0
+                if (!this.hero.defend) {
+                    this.hero.life -= dmg
+                    if(this.hero.life < 0) {
+                        this.hero.life = 0
+                    }
+                } else {
+                    this.hero.defenseArmor -= 1
+                    if(this.hero.defenseArmor == 0) {
+                        this.hero.defend = false
+                    }
                 }
             }
         }
